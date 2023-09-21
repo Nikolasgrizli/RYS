c9e6ef9fd157fd86ac0efa8c3b3b9863766d0cef
Mykola Yevsiukov <nikolasgrizli@gmail.com>
2023-09-21 18:55:49 +0200

    ready mobile category mixin

--- src/js/component/categories.js
+++ src/js/component/categories.js
@@ -1,16 +1,74 @@
 const categoriesWrapper = document.getElementById("categoriesWrapper");
 
 if (categoriesWrapper) {
-	const 	categories = categoriesWrapper.querySelectorAll(".js-category");
-	console.log(categories.length);
-	categories.forEach((category) => {
-		const 	categoryBtn = category.querySelector(".rubricator__btn"),
-				categoryWrapper = category.closest(".rubricator__item");
+	const categories = categoriesWrapper.querySelectorAll(".js-category"),
+		modalFilterBtn = document.querySelector(".js-modal-filter"),
+		rubricator = document.querySelector(".rubricator"),
+		isMobile = window.matchMedia("(max-width: 767px)").matches,
+		btnBack = document.querySelector(".js-rubricator-back"),
+		mainTitle = document.querySelector(".js-rubricator-title .r-title__child"),
+		btnReset = document.querySelector(".js-rubricator-reset"),
+		btnApply = document.querySelector(".js-rubricator-apply");
 
-		if(!categoryBtn) return;
+
+	let currentCategory = null;
+
+	categories.forEach((category) => {
+		const categoryBtn = category.querySelector(".rubricator__btn"),
+			categoryWrapper = category.closest(".rubricator__item"),
+			categoryTitle = category.querySelector(".rubricator__title");
+
+		if (!categoryBtn) return;
 		categoryBtn.addEventListener("click", () => {
-			categoryWrapper.classList.toggle("is-open");
+			if (isMobile) {
+				categoryWrapper.classList.add("is-open");
+				mainTitle.innerHTML = categoryTitle.innerHTML;
+				btnBack.classList.add("open-child");
+				currentCategory = categoryWrapper;
+			} else {
+				categoryWrapper.classList.toggle("is-open");
+			}
 		});
 	});
 
+
+	modalFilterBtn.addEventListener("click", (e) => {
+		e.preventDefault();
+		rubricator.classList.add("rubricator-open");
+		document.body.classList.add("modal-open");
+	});
+
+	btnBack.addEventListener("click", (e) => {
+		e.preventDefault();
+		if (btnBack.classList.contains("open-child")) {
+			console.log(currentCategory);
+
+			if (currentCategory) {
+				currentCategory.classList.remove("is-open");
+				const parentCategory = currentCategory.closest(".rubricator__item.is-open");
+				if (parentCategory) {
+					currentCategory = parentCategory;
+					mainTitle.innerHTML = parentCategory.querySelector(".rubricator__title").innerHTML;
+				} else {
+					currentCategory = null;
+				}
+
+				if (!currentCategory) {
+					btnBack.classList.remove("open-child");
+				}
+			}
+		} else {
+			rubricator.classList.remove("rubricator-open");
+			document.body.classList.remove("modal-open");
+		}
+	});
+
+	btnReset.addEventListener("click", (e) => {
+		e.preventDefault();
+		console.log("reset");
+	});
+	btnApply.addEventListener("click", (e) => {
+		e.preventDefault();
+		console.log("apply");
+	});
 }
